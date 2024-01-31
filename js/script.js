81ce52aaf91643a91b96cad1e2314c99af865755
federico_viganò <fede_viga@hotmail.it>
2024-01-31 16:00:52 +0100

    fine bonus - esercizio finito

--- js/script.js
+++ js/script.js
@@ -4,31 +4,42 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
+// inizializzo la costante gridElement
+const gridElement = document.querySelector("#grid");
 
 // scrivo il ciclo for per stampare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
+    // creo un nuovo elemento "newElement" di classe "square" e lo inserisco all'interno di "gridElement"
+    const newElement = document.createElement("div");
+    newElement.className = "square" ;
+    gridElement.append(newElement);
+
     // se i non è divisibile ne per 3 ne per 5 lo stampo
     if (i % 3 != 0 && i % 5 != 0) {
-        
-    console.log(i)
+
+        newElement.innerHTML = i;
+        newElement.style.backgroundColor = "red";
 
     // altrimenti controllo se è divisibile solo per 3, solo per 5 o per entrambi
     } else {
 
         // se è divisibile per 3 ma non per 5 stampo Fizz
         if (i % 3 == 0 && i % 5 != 0) {
-            console.log("Fizz")
+            newElement.innerHTML = "Fizz";
+            newElement.style.backgroundColor = "green";
         } 
         
         // se è divisibile per 5 ma non per 3 stampo Buzz
         if (i % 5 == 0 && i % 3 != 0) {
-            console.log("Buzz")
+            newElement.innerHTML = "Buzz";
+            newElement.style.backgroundColor = "blue";
         }
         
         // se è divisibile sia per 5 che per 3 stampo FizzBuzz
         if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz")
+            newElement.innerHTML = "Fizz Buzz";
+            newElement.style.backgroundColor = "purple";
         }
     }
     
